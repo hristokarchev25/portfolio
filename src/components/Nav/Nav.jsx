19b074032351f989eefb93b693796cb8f17db27e
Hristo Karchev <x_karchev@mail.bg>
2022-11-04 11:53:15 +0200

    update/ icons added to navigation

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./nav.css";
+import { ImHome3 } from "react-icons/im";
+import { FaUserAlt } from "react-icons/fa";
+import { ImBook } from "react-icons/im";
+import { RiServiceFill } from "react-icons/ri";
+import { RiMessage2Fill } from "react-icons/ri";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#home">
+        <ImHome3 />
+      </a>
+      <a href="#about">
+        <FaUserAlt />
+      </a>
+      <a href="#experience">
+        <ImBook />
+      </a>
+      <a href="#services">
+        <RiServiceFill />
+      </a>
+      <a href="#contact">
+        <RiMessage2Fill />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;

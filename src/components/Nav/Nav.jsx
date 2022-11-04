818d1f7c309a4a73657d2ec54b05361ff1761399
Hristo Karchev <x_karchev@mail.bg>
2022-11-04 12:29:08 +0200

    fix/ navigation active class with useState

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,23 +5,46 @@ import { FaUserAlt } from "react-icons/fa";
 import { ImBook } from "react-icons/im";
 import { RiServiceFill } from "react-icons/ri";
 import { RiMessage2Fill } from "react-icons/ri";
+import { useState } from "react";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <nav>
-      <a href="#home" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <ImHome3 />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <FaUserAlt />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <ImBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
         <RiServiceFill />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <RiMessage2Fill />
       </a>
     </nav>

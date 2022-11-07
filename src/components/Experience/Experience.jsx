27f268d92ed55e793bbef3921a1b873422165e73
Hristo Karchev <x_karchev@mail.bg>
2022-11-07 19:37:03 +0200

    fix/ code reusability by creating data js file

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import "./experience.css";
-import { BsFillCheckCircleFill } from "react-icons/bs";
+import ExperienceDetailsArticle from "./ExperienceDetails/ExperienceDetailsArticle";
+import {
+  frontendObjOne,
+  frontendObjTwo,
+  frontendObjThree,
+  frontendObjFour,
+  frontendObjFive,
+  backendObjOne,
+  backendObjTwo,
+  backendObjThree,
+  backendObjFour,
+  backendObjFive,
+} from "./ExperienceDetails/dataOfTechnologies";
 
 const Experience = () => {
   return (
@@ -11,62 +23,22 @@ const Experience = () => {
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
-            </article>
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>CSS</h4>
-              <small className="text-light">Intermediate</small>
-            </article>
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>JavaScript</h4>
-              <small className="text-light">Experienced</small>
-            </article>
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>Vue.js</h4>
-              <small className="text-light">Experienced</small>
-            </article>
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>React</h4>
-              <small className="text-light">Experienced</small>
-            </article>
+            <ExperienceDetailsArticle {...frontendObjOne} />
+            <ExperienceDetailsArticle {...frontendObjTwo} />
+            <ExperienceDetailsArticle {...frontendObjThree} />
+            <ExperienceDetailsArticle {...frontendObjFour} />
+            <ExperienceDetailsArticle {...frontendObjFive} />
           </div>
         </div>
 
         <div className="experience__backend">
-        <h3>Backend Development</h3>
+          <h3>Backend Development</h3>
           <div className="experience__content">
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>Express.JS</h4>
-              <small className="text-light">Experienced</small>
-            </article>
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>MongoDB</h4>
-              <small className="text-light">Intermediate</small>
-            </article>
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>MySQL</h4>
-              <small className="text-light">Experienced</small>
-            </article>
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>Python</h4>
-              <small className="text-light">Experienced</small>
-            </article>
-            <article className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>Node.js</h4>
-              <small className="text-light">Experienced</small>
-            </article>
+            <ExperienceDetailsArticle {...backendObjOne} />
+            <ExperienceDetailsArticle {...backendObjTwo} />
+            <ExperienceDetailsArticle {...backendObjThree} />
+            <ExperienceDetailsArticle {...backendObjFour} />
+            <ExperienceDetailsArticle {...backendObjFive} />
           </div>
         </div>
       </div>
---
+++ src/components/Experience/ExperienceDetails/dataOfTechnologies.js
@@ -0,0 +1,49 @@
+export const frontendObjOne = {
+    technologyName: "HTML",
+    levelOfExperience: "Experienced"
+};
+
+export const frontendObjTwo = {
+    technologyName: "CSS",
+    levelOfExperience: "Intermediate"
+};
+
+export const frontendObjThree = {
+    technologyName: "JavaScript",
+    levelOfExperience: "Experienced"
+};
+
+export const frontendObjFour = {
+    technologyName: "Vue.js",
+    levelOfExperience: "Intermediate"
+};
+
+export const frontendObjFive = {
+    technologyName: "React",
+    levelOfExperience: "Experienced"
+};
+
+export const backendObjOne = {
+    technologyName: "Express.JS",
+    levelOfExperience: "Experienced"
+};
+
+export const backendObjTwo = {
+    technologyName: "MongoDB",
+    levelOfExperience: "Experienced"
+};
+
+export const backendObjThree = {
+    technologyName: "MySQL",
+    levelOfExperience: "Intermediate"
+};
+
+export const backendObjFour = {
+    technologyName: "Python",
+    levelOfExperience: "Intermediate"
+};
+
+export const backendObjFive = {
+    technologyName: "Node.js",
+    levelOfExperience: "Experienced"
+};

7dcb7287038b011b6082486ebb0ca937604747df
Hristo Karchev <x_karchev@mail.bg>
2022-11-21 20:44:10 +0200

    fix/ form sending messages

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -12,21 +12,15 @@ const Contacts = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "YOUR_SERVICE_ID",
-        "YOUR_TEMPLATE_ID",
-        form.current,
-        "YOUR_PUBLIC_KEY"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    emailjs.sendForm(
+      "service_h0wwt9m",
+      "template_dkrhfgf",
+      form.current,
+      "F6DpDCZFuE7vcLJRP"
+    );
+
+    e.target.reset();
+    alert("The message was sent successfully");
   };
 
   return (

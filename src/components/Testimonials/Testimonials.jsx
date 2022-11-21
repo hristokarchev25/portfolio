3264362465070c70516a0122855a43785b922785
Hristo Karchev <x_karchev@mail.bg>
2022-11-21 12:06:37 +0200

    add/ hardcoded testimonials jsx

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./testimonials.css";
+import AVATAR from "../../assets/ico.jpg";
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Reviews from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR} alt="avatar" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
+            molestiae dolorum ad alias, omnis error, sed quisquam neque, vero ut
+            similique quam modi delectus rem architecto quia.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR} alt="avatar" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
+            molestiae dolorum ad alias, omnis error, sed quisquam neque, vero ut
+            similique quam modi delectus rem architecto quia.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR} alt="avatar" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
+            molestiae dolorum ad alias, omnis error, sed quisquam neque, vero ut
+            similique quam modi delectus rem architecto quia.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR} alt="avatar" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
+            molestiae dolorum ad alias, omnis error, sed quisquam neque, vero ut
+            similique quam modi delectus rem architecto quia.
+          </small>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
